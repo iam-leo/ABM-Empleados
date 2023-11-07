2a1a3cc2d71afc8e7eedf52dc4010c25c2f9ebce
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-11-07 20:04:20 -0300

    add: main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEditEmployeeComponent } from './components/add-edit-employee/add-edit-employee.component';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ConfirmMessageComponent } from './components/shared/confirm-message/confirm-message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEditEmployeeComponent,
+    ListEmployeesComponent,
+    NavbarComponent,
+    ConfirmMessageComponent
   ],
   imports: [
     BrowserModule,
