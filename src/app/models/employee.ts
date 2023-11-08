a5762164d8f4bde4883e45653ff05d6b38623fd9
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-11-08 09:51:07 -0300

    add: employee model

---
+++ src/app/models/employee.ts
@@ -0,0 +1,8 @@
+export class Employees {
+    name: string = '';
+    gender: string = '';
+    phone: number = 0;
+    email: string = '';
+    dateEntry: Date = new Date();
+    matrialStatus: string = '';
+}
