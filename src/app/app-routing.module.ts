035e25ec280284bff45ec0e9fc5bdec571bc8e2d
Javier Leonel Sosa <leo.vm.cba@gmail.com>
2023-11-14 09:50:35 -0300

    add: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEditEmployeeComponent } from './components/add-edit-employee/add-edit-employee.component';
+import { ListEmployeesComponent } from './components/list-employees/list-employees.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListEmployeesComponent },
+  { path: 'add', component: AddEditEmployeeComponent },
+  { path: 'edit/:id', component: AddEditEmployeeComponent },
+  { path: '**', component: ListEmployeesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
